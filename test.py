bc3fa217aba176338cb94f0687f1afabb9e682da
Mahed Javed <mahed95@gmail.com>
2022-09-22 10:10:43 +0100

    added test.py

--- test.py
+++ test.py
@@ -2,17 +2,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-as = 3
+randNo = 3
 
 
-def mate(bitch):
+def testmate(test):
     a = 7
     b = 1
     for i in range(1):
-        print("my shorts are dead")
+        print("my zombies are dead")
         a = 4
-
-    apm = 1314
-    make_lovr = 123
-    ad = 13
-    amdoa =131;
