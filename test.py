a4de3d531dfafd4f4b32bbdb2f3b62343e303e68
Mahed Javed <mahed95@gmail.com>
2022-09-22 10:09:06 +0100

    added kata 3

---
+++ test.py
@@ -0,0 +1,18 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+as = 3
+
+
+def mate(bitch):
+    a = 7
+    b = 1
+    for i in range(1):
+        print("my shorts are dead")
+        a = 4
+
+    apm = 1314
+    make_lovr = 123
+    ad = 13
+    amdoa =131;
